7b91766f8abe9fff4733c77a65a124db66d09886
Zuha Ali <zuhaali988@gmail.com>
2025-03-20 15:47:37 +0500

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioDetailsComponent } from './pages/portfolio-details/portfolio-details.component';
+import { ServiceDetailsComponent } from './pages/service-details/service-details.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
